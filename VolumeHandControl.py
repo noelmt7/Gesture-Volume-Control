cd1b977a15b26d45943b772ce6466d062e230de5
noelmt7 <noel.thomas@msam.christuniversity.in>
2024-07-24 11:46:17 +0530

    gui

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -7,53 +7,51 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-wCam, hCam = 640, 480
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
-pTime = 0
+class HandVolumeControl:
+    def __init__(self, wCam=640, hCam=480, detectionCon=0.7):
+        self.wCam, self.hCam = wCam, hCam
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(3, self.wCam)
+        self.cap.set(4, self.hCam)
+        self.pTime = 0
 
-detector = htm.handDetector(detectionCon=0.7)
+        self.detector = htm.handDetector(detectionCon=detectionCon)
 
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = interface.QueryInterface(IAudioEndpointVolume)
-# volume.GetMute()
-# volume.GetMasterVolumeLevel()
-volRange = volume.GetVolumeRange()
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        self.volume = interface.QueryInterface(IAudioEndpointVolume)
+        self.volRange = self.volume.GetVolumeRange()
 
-minVol = volRange[0]
-maxVol = volRange[1]
+        self.minVol = self.volRange[0]
+        self.maxVol = self.volRange[1]
 
-while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-    if len(lmList) != 0:
-        # print(lmList[4], lmList[8])
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+    def process_frame(self):
+        success, img = self.cap.read()
+        if not success:
+            return None  # Return None if frame is not captured successfully
 
-        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-        cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
-        length = math.hypot(x2 - x1, y2 - y1)
-        print(length)
+        img = cv2.flip(img, 1)
+        img = self.detector.findHands(img)
+        lmList = self.detector.findPosition(img, draw=False)
+        if len(lmList) != 0:
+            x1, y1 = lmList[4][1], lmList[4][2]
+            x2, y2 = lmList[8][1], lmList[8][2]
+            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-        # Hand range was 50-300, adjust to 50-400 for larger distance
-        # Volume range -65 to 0
-        vol = np.interp(length, [50, 400], [minVol, maxVol])
-        print(int(length), vol)
-        volume.SetMasterVolumeLevel(vol, None)
+            cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+            length = math.hypot(x2 - x1, y2 - y1)
+            vol = np.interp(length, [50, 400], [self.minVol, self.maxVol])
+            self.volume.SetMasterVolumeLevel(vol, None)
 
-        if length < 50:
-            cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+            if length < 50:
+                cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
-    cv2.imshow("Img", img)
-    cv2.waitKey(1)
+        cTime = time.time()
+        fps = 1 / (cTime - self.pTime)
+        self.pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+        return img
